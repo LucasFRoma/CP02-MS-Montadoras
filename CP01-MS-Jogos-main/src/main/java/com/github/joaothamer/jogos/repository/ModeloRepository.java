d30b4e69609765b45500a103ba146a7676b254ce
Lucas Furquim C. Roma <lucas.furquim.roma@gmail.com>
2026-05-04 21:04:43 -0300

    Delete CP01-MS-Jogos-main directory

--- CP01-MS-Jogos-main/src/main/java/com/github/joaothamer/jogos/repository/ModeloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.joaothamer.jogos.repository;
-
-import com.github.joaothamer.jogos.model.Modelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModeloRepository extends JpaRepository<Modelo, Long> {
-}
--- CP01-MS-Jogos-main/src/main/java/com/github/joaothamer/jogos/repository/MontadoraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.joaothamer.jogos.repository;
-
-import com.github.joaothamer.jogos.model.Montadora;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MontadoraRepository extends JpaRepository<Montadora, Long> {
-}
